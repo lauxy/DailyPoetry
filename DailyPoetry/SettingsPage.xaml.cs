8cdd3e4a03a80e616e5211649b521b50eb6e6ba5
LauXy <1023806418@qq.com>
2019-04-13 16:24:48 +0800

    基本完成了SettingsPage的前端设计

--- DailyPoetry/SettingsPage.xaml.cs
+++ DailyPoetry/SettingsPage.xaml.cs
@@ -26,5 +26,91 @@ namespace DailyPoetry
         {
             this.InitializeComponent();
         }
+
+        private void ToggleSwitch_OnToggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    toggleSwitch.OffContent = "开";
+                }
+                else
+                {
+                    toggleSwitch.OffContent = "关";
+                }
+            }
+        }
+
+        private void LockScreenSwitch_OnToggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    toggleSwitch.OffContent = "开";
+                }
+                else
+                {
+                    toggleSwitch.OffContent = "关";
+                }
+            }
+        }
+
+        /// <summary>
+        /// 应用程序采用浅色主题。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void LightColorRadioButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 应用程序采用深色主题。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DarkColorRadioButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            this.RequestedTheme = ElementTheme.Dark;
+        }
+
+        /// <summary>
+        /// 应用程序与系统主题保持一致。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SystemColorRadioButton_OnChecked(object sender, RoutedEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 选择“我的创作”界面的背景是否采用默认背景。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackgroundSwitch_OnToggled(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch toggleSwitch = sender as ToggleSwitch;
+            if (toggleSwitch != null)
+            {
+                if (toggleSwitch.IsOn == true)
+                {
+                    toggleSwitch.OffContent = "开";
+                }
+                else
+                {
+                    toggleSwitch.OffContent = "关";
+                }
+            }
+        }
+
+     
     }
 }
